ee5e46045ae0c4e61ad8b04430ec0d2e035f6d70
Adam Watkins <adam@stupidpupil.co.uk>
2023-06-02 12:46:41 +0100

    Remove product_type filter

--- R/get_blue_horizon_products.R
+++ R/get_blue_horizon_products.R
@@ -1,10 +1,6 @@
 get_blue_horizon_products <- function(){
 	products <- jsonlite::read_json("https://bluehorizonbloodtests.co.uk/products.json?limit=250")$products
 
-	products <- products |>
-    purrr::keep(function(p){p$product_type %in% c("Blood Test", "")})
-
-
   products <- products |>
     purrr::map(function(p){list_from_blue_horizon_product_json(p)})
 
